cebd1aeef4bae86305f529f1f00e9364fdb7b014
ChristianFromBoston <79617518+ChristianFromBoston@users.noreply.github.com>
2023-03-20 00:07:52 -0400

    Update btreelookup.js

--- btreelookup.js
+++ btreelookup.js
@@ -1,22 +1,22 @@
 function submitForm() {
-  var streetNumber = $('#street-number').val();
-  var streetName = $('#street-name').val();
-  console.log('Submitting form with streetNumber:', streetNumber, 'and streetName:', streetName);
-  $.ajax({
-    url: 'https://cors-anywhere.herokuapp.com/https://braintree.patriotproperties.com/search-middle-ns.asp',
-    method: 'POST',
-    data: {
-      'SearchStreetNumber': streetNumber,
-      'SearchStreetName': streetName
-    },
-    dataType: 'html'
-  }).done(function(response) {
-    var propertyValue = $(response).find('td:contains("$")').text().trim();
-    console.log('Response received:', response);
-    console.log('Property value extracted:', propertyValue);
-    $('#result').text('The property value is ' + propertyValue);
-  }).fail(function(jqXHR, textStatus, errorThrown) {
-    console.error('Error occurred during AJAX request:', errorThrown);
-    $('#result').text('Error: ' + errorThrown);
-  });
+    var streetNumber = $('#street-number').val();
+    var streetName = $('#street-name').val();
+    console.log('Submitting form with street number:', streetNumber, 'and street name:', streetName);
+    $.ajax({
+        url: 'https://cors-anywhere.herokuapp.com/https://braintree.patriotproperties.com/search-middle-ns.asp',
+        method: 'POST',
+        data: {
+            'SearchStreetNumber': streetNumber,
+            'SearchStreetName': streetName
+        },
+        dataType: 'html'
+    }).done(function(response) {
+        console.log('Received response:', response);
+        var propertyValue = $(response).find('td:contains("$")').text().trim();
+        console.log('Found property value:', propertyValue);
+        $('#result').text('The property value is ' + propertyValue);
+    }).fail(function(jqXHR, textStatus, errorThrown) {
+        console.error('Error:', errorThrown);
+        $('#result').text('Error: ' + errorThrown);
+    });
 }

311bd7c02f041073a672bb0488c235addacba3ba
ChristianFromBoston <79617518+ChristianFromBoston@users.noreply.github.com>
2023-03-20 00:13:52 -0400

    Update btreelookup.js

--- btreelookup.js
+++ btreelookup.js
@@ -1,20 +1,15 @@
-function submitForm() {
-    var streetNumber = $('#street-number').val();
-    var streetName = $('#street-name').val();
-    $.ajax({
-        url: 'https://cors-anywhere.herokuapp.com/https://braintree.patriotproperties.com/search-middle-ns.asp',
-        method: 'POST',
-        data: {
-            'SearchStreetNumber': streetNumber,
-            'SearchStreetName': streetName
-        },
-        dataType: 'html'
-    }).done(function(response) {
-        console.log('Response:', response); // add this line to log the response
-        var propertyValue = $(response).find('td:contains("$")').text().trim();
-        $('#result').text('The property value is ' + propertyValue);
-    }).fail(function(jqXHR, textStatus, errorThrown) {
-        console.log('Error:', errorThrown); // add this line to log the error
-        $('#result').text('Error: ' + errorThrown);
-    });
-}
+$.ajax({
+    url: 'https://cors-anywhere.herokuapp.com/https://braintree.patriotproperties.com/search-middle-ns.asp',
+    method: 'POST',
+    data: {
+        'SearchStreetNumber': streetNumber,
+        'SearchStreetName': streetName
+    },
+    dataType: 'html',
+    timeout: 5000 // set timeout to 5 seconds
+}).done(function(response) {
+    var propertyValue = $(response).find('td:contains("$")').text().trim();
+    $('#result').text('The property value is ' + propertyValue);
+}).fail(function(jqXHR, textStatus, errorThrown) {
+    $('#result').text('Error: ' + errorThrown);
+});
